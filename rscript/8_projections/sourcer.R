c873cf9d13b67b1bbf8492fc2862e86989c88c26
Giacomo Falchetta <36954873+giacfalk@users.noreply.github.com>
2025-02-28 14:31:59 +0100

    Update sourcer.R

--- rscript/8_projections/sourcer.R
+++ rscript/8_projections/sourcer.R
@@ -1,15 +1,15 @@
 
 # national projections, national models, without social drivers
-lapply(list.files("C:/Users/falchetta/Downloads/acglobal/rscript/8_projections/national_model/only_macro_drivers/", pattern = "script", recursive=F, full.names = T), source, echo=T)
+lapply(list.files("C:/Users/Utente/Downloads/acglobal/rscript/8_projections/national_model/only_macro_drivers/", pattern = "script", recursive=F, full.names = T), source, echo=T)
 
 # national projections, national models
-lapply(list.files("C:/Users/falchetta/Downloads/acglobal/rscript/8_projections/national_model/", pattern = "_dmf", recursive=F, full.names = T), source, echo=T)
+lapply(list.files("C:/Users/Utente/Downloads/acglobal/rscript/8_projections/national_model/", pattern = "_dmf", recursive=F, full.names = T), source, echo=T)
 
 # # national projections, global model, without social drivers
 lapply(list.files("C:/Users/Utente/Downloads/acglobal/rscript/8_projections/global_model/only_macro_drivers", pattern = "script", recursive=F, full.names = T), source, echo=T)
 
 # national projections, global model
-lapply(list.files("C:/Users/falchetta/Downloads/acglobal/rscript/8_projections/global_model/", pattern = "script", recursive=F, full.names = T)[-6], source, echo=T)
+lapply(list.files("C:/Users/Utente/Downloads/acglobal/rscript/8_projections/global_model/", pattern = "script", recursive=F, full.names = T)[-6], source, echo=T)
 
 # # global projections, global model
-lapply(list.files("C:/Users/Utente/Downloads/acglobal/rscript/8_projections/global_model/only_macro_drivers", pattern = "script", recursive=F, full.names = T)[6], source, echo=T)
+lapply(list.files("C:/Users/Utente/Downloads/acglobal/rscript/8_projections/global_model/", pattern = "script", recursive=F, full.names = T)[6], source, echo=T)
